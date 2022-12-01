b610f53cda78172cf2393a2cd39383504e01d7f9
Adoo7 <109992569+Adoo7@users.noreply.github.com>
2022-12-01 10:41:18 +0300

    initial push
    first push with non-functional main menu

---
+++ FitnessApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  FitnessApp
+//
+//  Created by ahmed sarhan on 30/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var DetailsAboutUser: UITextView!
+    
+    
+    @IBAction func testFunction(_ sender: Any) {
+    }
+    
+}
